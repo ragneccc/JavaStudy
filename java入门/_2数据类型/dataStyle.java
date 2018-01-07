b45400331c6cbec42de6bd760cab986c72c30fef
ragneccc@163.com <ragneccc@163.com>
2018-01-07 15:57:43 +0800

    数据类型检点练习

---
+++ java入门/_2数据类型/dataStyle.java
@@ -0,0 +1,20 @@
+public class dataStyle {
+	public static void main(String []args){
+		char c = 'a';
+		int i = 10 ;
+		boolean b =	false ;	//OR ture
+		float f;
+		double d = 43.5 ;
+		byte ui =6 ;
+		i += c ;
+		System.out.println(i);
+		d = d - 2;
+		System.out.print(d );
+		i=c*(byte)i;
+		System.out.println(i);
+		i = 100%ui;
+		d  = 100/ui ;
+		System.out.println(i);
+		System.out.print(d );
+	}
+}  
